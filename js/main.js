c786d2d942e4c25b57d5a995128dbdce70035510
Boris <glimah481@gmail.com>
2024-10-15 20:26:12 +0300

    исправления

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 import {createPhoto} from './createPhoto.js';
 import {NUMBER_OF_PHOTOS} from './constants.js';
 
-
 const mockedPhotos = Array.from({ length: NUMBER_OF_PHOTOS }, createPhoto);
 
 export{mockedPhotos};
