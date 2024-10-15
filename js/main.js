8cbefa7c04f4535fd7c15283392bd1b7ddce1635
Boris <glimah481@gmail.com>
2024-10-15 20:31:28 +0300

    Отрисуй меня полностью

--- js/main.js
+++ js/main.js
@@ -2,12 +2,7 @@ import {createPhoto} from './createPhoto.js';
 import {NUMBER_OF_PHOTOS} from './constants.js';
 
 
-//function getSimilarPhotos(){
 const mockedPhotos = Array.from({ length: NUMBER_OF_PHOTOS }, createPhoto);
-//   return similarPhotos;
-// }
-//getSimilarPhotos();
 
-//export{getSimilarPhotos};
 export{mockedPhotos};
 
