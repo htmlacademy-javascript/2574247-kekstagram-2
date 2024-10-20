c1237cd4ca25baa9c3d8630eedcd87c14517e321
Keks <keks@htmlacademy.ru>
2024-10-20 18:58:11 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/createPhoto.js
@@ -0,0 +1,20 @@
+import {getRandomArrayElement, getRandomInteger} from './utils.js';
+import {DESCRIPTION,LIKES_MIN,LIKES_MAX,COMMENTS_MIN,COMMENTS_MAX} from './constants.js';
+import {generateComment} from './generateComment.js';
+let photoIdx = 0;
+
+function createPhoto() {
+  const description = getRandomArrayElement(DESCRIPTION);
+  const likes = getRandomInteger(LIKES_MIN, LIKES_MAX);
+  const commentsCount = getRandomInteger(COMMENTS_MIN, COMMENTS_MAX);
+  const existingCommentIds = [];
+  const comments = Array.from({ length: commentsCount }, () => generateComment(existingCommentIds));
+
+  return {
+    url: `photos/${(photoIdx += 1)}.jpg`,
+    description,
+    likes,
+    comments,
+  };
+}
+export{createPhoto};
