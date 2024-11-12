7909027c2d92749a5bbfc528e7af1e112ee20467
Boris <glimah481@gmail.com>
2024-11-12 19:39:15 +0300

    fix

--- js/upload-form.js
+++ js/upload-form.js
@@ -1,4 +1,5 @@
 import {isEscapeKey} from './utils.js';
+import { FILE_TYPES } from './constants.js';
 const body = document.querySelector('body');
 const uploadForm = document.querySelector('.img-upload__form');
 const hashtagInput = uploadForm.querySelector('.text__hashtags');
@@ -7,6 +8,24 @@ const photoEditorForm = uploadForm.querySelector('.img-upload__overlay');
 const photoEditorResetBtn = photoEditorForm.querySelector('#upload-cancel');
 const commentInput = uploadForm.querySelector('.text__description');
 const defaultPhoto = document.querySelector('.img-upload__preview img');
+const fileSelector = uploadForm.querySelector('.img-upload__input');
+const effectsPreview = uploadForm.querySelectorAll('.effects__preview');
+
+const isLoadPhoto = () => {
+  const file = fileSelector.files[0];
+  const fileName = file.name.toLowerCase();
+  const fileExt = fileName.split('.').pop();
+  const matches = FILE_TYPES.includes(fileExt);
+  if(matches){
+    const url = URL.createObjectURL(file);
+    defaultPhoto.src = url;
+    effectsPreview.forEach((effect) => {
+      effect.style.backgroundImage = `url(${url})`;
+    });
+  } else {
+    closePhotoEditor();
+  }
+};
 
 const onPhotoEditorResetBtnClick = () => closePhotoEditor();
 
@@ -32,11 +51,12 @@ function closePhotoEditor(){
   uploadForm.reset();
 }
 
-uploadFileControl.addEventListener('change', ()=>{
+const onUploadFormShow = ()=>{
   photoEditorForm.classList.remove('hidden');
-  body.classList.add('.modal-open');
+  body.classList.add('modal-open');
+  isLoadPhoto();
   photoEditorResetBtn.addEventListener('click', onPhotoEditorResetBtnClick);
   document.addEventListener('keydown', onDocumentKeydown);
-});
-
+};
+uploadFileControl.addEventListener('change', onUploadFormShow);
 export{closePhotoEditor, body, uploadForm, defaultPhoto};
